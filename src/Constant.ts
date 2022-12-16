698c76ed2531b9f19468bc29f1dc9ba854acd2d6
thetrung <deulamco@gmail.com>
2022-12-17 03:06:42 +0700

    remove unused constant

--- src/Constant.ts
+++ src/Constant.ts
@@ -1,13 +1,8 @@
 // initial balance ?
 const initialPayerBalance = 1_000_000_000;
-// initial
-const initialAccountBalance = 0; // zero
 
 export class Constant {
   static get initialPayerBalance() {
     return initialPayerBalance;
-  }
-  static get initialAccountBalance() {
-    return initialAccountBalance;
   }
 }
